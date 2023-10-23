1f9326cbfa3c92b4fecb0816bf9a02eeba857d69
Sadir Sánchez Valderrama <sadir.sanchez@gmail.com>
2023-10-22 20:49:30 -0500

    Se definen las solicitudes POST para el formulario receive.vue

--- controllers/receive/ReceiveController.php
+++ controllers/receive/ReceiveController.php
@@ -1,10 +1,23 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
 
-if (isset($_POST["name"])){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    echo "Recibido";
+    $input = file_get_contents("php://input");
+    $data = json_decode($input, true);
+    $dependency = $data['dependency'];
+    echo "Dependencia: $dependency";
+    $documentType = $data['documentType'];
+    echo "Tipo de documento: $documentType";
+    $name = $data['name'];
+    echo "Nombre: $name";
+    $code = $data['code'];
+    echo "Código: $code";
+    $selectedDatePicker = $data['selectedDatePicker'];
+    echo "Fecha de elaboración: $selectedDatePicker";
+    $inventory = $data['inventory'];
+    echo "Inventario: $inventory";
+    $location = $data['location'];
+    echo "Ubicación en físico: $location";
 }
-
-
-
-?>
