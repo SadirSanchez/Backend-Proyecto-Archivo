a188801423e4377f3628fbb698226d3804c597a0
Sadir Sánchez Valderrama <sadir.sanchez@gmail.com>
2023-11-02 14:21:26 -0500

    Se crea archivo para la autenticación de los usuarios LoginController.php

---
+++ controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+require("conection.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = file_get_contents("php://input");
+    $data = json_decode($input, true);
+
+    $nameUser = $data['nameUser'];
+    $password = $data['password'];
+
+    // Conecta a la base de datos
+    $conection = new Conection();
+    $conn = $conection->conect();
+
+    // Verifica la conexión
+    if ($conn->connect_error) {
+        echo "Conexión fallida: " . $conn->connect_error;
+    } else {
+        // Realiza la consulta para verificar las credenciales del usuario
+        $query = "SELECT * FROM users WHERE NameUser='$nameUser' AND Id='$password'";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+            echo "Autenticación exitosa"; // Las credenciales son válidas
+        } else {
+            echo "Credenciales inválidas"; // Las credenciales son incorrectas
+        }
+
+        $conn->close();
+    }
+}
