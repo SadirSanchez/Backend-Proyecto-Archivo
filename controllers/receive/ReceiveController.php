b66dab16b2595b889cba15b481733390d4a8787c
SadirSÃanchez <sadir.sanchez@gmail.com>
2023-11-27 09:44:01 -0500

    Queda funcional el registro de información de documentos en la DB-proyecto_archivo

--- controllers/receive/ReceiveController.php
+++ controllers/receive/ReceiveController.php
@@ -2,22 +2,35 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 
+require("../conection.php");
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $input = file_get_contents("php://input");
-    $data = json_decode($input, true);
-    $dependency = $data['dependency'];
-    echo "Dependencia: $dependency";
-    $documentType = $data['documentType'];
-    echo "Tipo de documento: $documentType";
-    $name = $data['name'];
-    echo "Nombre: $name";
-    $code = $data['code'];
-    echo "Código: $code";
-    $selectedDatePicker = $data['selectedDatePicker'];
-    echo "Fecha de elaboración: $selectedDatePicker";
-    $inventory = $data['inventory'];
-    echo "Inventario: $inventory";
-    $location = $data['location'];
-    echo "Ubicación en físico: $location";
+    $conection = new Conection();
+    if ($conection->validate() === 'ok') {
+
+        $input = file_get_contents("php://input");
+        $data = json_decode($input, true);
+
+        $dependency = $data['dependency'];
+        $documentType = $data['documentType'];
+        $name = $data['name'];
+        $time = $data['time'];
+        $selectedDatePicker = $data['selectedDatePicker'];
+        $inventory = $data['inventory'];
+        $location = $data['location'];
+
+        $conn = $conection->conect();
+        $sql = "INSERT INTO document (DocumentName,DocumentType,DateElaboration,TotalInventory,RetentionTime,OriginDependency,PhysicalLocation) VALUES 
+        ('$name','$documentType','$selectedDatePicker','$inventory','$time','$dependency','$location')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Registro insertado correctamente.";
+        } else {
+            echo "Error al insertar el registro: " . $conn->error;
+        }
+
+    }
 }
+
+?>
