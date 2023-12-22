ac71a41c80bb4846db7b8417a5a81a229a4748c2
SadirSÃanchez <sadir.sanchez@gmail.com>
2023-12-22 05:57:51 -0500

    Se cambia el nombre de la carpeta receive por document y dentro de esta se crea el archivo ConsultDocument en donde se realiza la consulta y se listan los documentos de la base de datos

---
+++ controllers/document/ConsultDocument.php
@@ -0,0 +1,48 @@
+<?php
+// Encabezados para permitir el acceso desde cualquier origen
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Se incluye el archivo de conexión
+require("../conection.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $conection = new Conection();
+    if ($conection->validate() === 'ok') {
+
+        $input = file_get_contents("php://input");
+        $data = json_decode($input, true);
+
+        $documentName = $data['documentName'];
+        $documentType = $data['documentType'];
+        $originDependency = $data['originDependency'];
+        $dateElaboration = $data['dateElaboration'];
+
+
+        $conn = $conection->conect();
+
+        // Verifica la conexión
+        if ($conn->connect_error) {
+            echo "Conexión fallida: " . $conn->connect_error;
+        } else {
+            // Realiza la consulta a la base de datos
+            $query = "SELECT * FROM document WHERE DocumentName='$documentName' or DocumentType='$documentType' or OriginDependency='$originDependency' or DateElaboration='$dateElaboration' ";
+            $result = $conn->query($query);
+
+            // Cierra la conexión
+            $conn->close();
+
+            $documents = $result->fetch_all(MYSQLI_ASSOC);
+
+            // Devuelve los usuarios como JSON
+            header('Content-Type: application/json');
+
+            echo json_encode($documents);
+
+        }
+    }
+
+}
+
+?>
