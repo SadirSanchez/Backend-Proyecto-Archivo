e4cb42aa4611cd6a5d999d8ba63b6b8accc7c1e6
SadirSÃanchez <sadir.sanchez@gmail.com>
2024-02-06 18:14:18 -0500

    Se configura con carbon para recibir fechas exactas y se modifica consulta a rangos

--- controllers/document/ConsultDocument.php
+++ controllers/document/ConsultDocument.php
@@ -1,4 +1,8 @@
 <?php
+require '../../ext/Carbon/autoload.php';
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+
 // Encabezados para permitir el acceso desde cualquier origen
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
@@ -19,6 +23,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $originDependency = $data['originDependency'];
         $dateElaboration = $data['dateElaboration'];
 
+        // Convertir las cadenas a objetos Carbon
+        $startDate = Carbon::parse($dateElaboration[0]);
+        $endDate = Carbon::parse($dateElaboration[1]);
+
+        // Ajustar las horas
+        $startDate->startOfDay(); // Establecer hora a las 00:00:00
+        $endDate->endOfDay();     // Establecer hora a las 23:59:59
 
         $conn = $conection->conect();
 
@@ -27,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "Conexión fallida: " . $conn->connect_error;
         } else {
             // Realiza la consulta a la base de datos
-            $query = "SELECT * FROM document WHERE DocumentName='$documentName' or DocumentType='$documentType' or OriginDependency='$originDependency' or DateElaboration='$dateElaboration' ";
+            $query = "SELECT * FROM document WHERE DocumentName='$documentName' OR DocumentType='$documentType' OR OriginDependency='$originDependency' OR (DateElaboration BETWEEN '$startDate' AND '$endDate')";
             $result = $conn->query($query);
 
             // Cierra la conexión
